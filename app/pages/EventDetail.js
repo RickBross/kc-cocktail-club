3df03c9d0640ff491c40d0552968c854c2344291
Rick Bross <rbross3@gmail.com>
2016-11-08 23:35:27 -0500

    add component

---
+++ app/pages/EventDetail.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Link } from 'react-router';
+import classNames from 'classnames'
+
+import Menu from '../components/Menu/Menu';
+import Slideshow from '../UI/Slideshow/Slideshow';
+import MenuStore from '../stores/MenuStore';
+import AppStore from '../stores/AppStore';
+
+import styles from './_EventDetail.scss';
+
+
+export default class EventDetail extends React.Component {
+
+
+  componentDidMount() {
+    document.title = "EventDetail - Kansas City Cocktail Club";
+
+    const bg = 'white'
+    AppStore.setProps({tone:'dark'});
+    MenuStore.setProps({title:'Discover'});
+
+    AppStore.setProps({background:bg});
+    AppStore.setProps({backgroundImage:'none'});
+    AppStore.setProps({backgroundSize:'auto 100%'});
+    AppStore.setProps({backgroundPosition:'0 0'});
+  }
+
+  render() {
+
+    return (
+      <div>
+        <Menu
+          title={MenuStore.getProp('title')}
+          tone={AppStore.getProp('tone')}
+          active={MenuStore.getProp('active')} />
+        <div className={classNames(styles.body, styles.events)}>
+            <div className={classNames(styles.wrapper, styles.makeBackground)}>
+              <Slideshow
+                tone={AppStore.getProp('tone')} />
+            </div>
+          </div>
+        </div>
+      );
+  }
+}
